c2c8fb32ac7f00df11f9441095beca302f8366d6
司芳源 <sify21@163.com>
2026-02-10 00:22:48 +0800

    f

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+vim.lsp.enable("basedpyright")
+vim.lsp.enable("lua_ls")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
