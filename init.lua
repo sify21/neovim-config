dd4da036b7dfd2ac39e20abe3f83d0ec424ddd4f
司芳源 <sify21@163.com>
2026-03-24 16:47:04 +0800

    telescope

--- init.lua
+++ init.lua
@@ -23,3 +23,11 @@ require('nvim-treesitter').install { 'rust', 'python', 'html', 'latex', 'yaml', 
 
 -- colorscheme
 vim.cmd("colorscheme catppuccin-nvim")
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fl', builtin.lsp_document_symbols, { desc = 'Telescope LSP document symbols' })
