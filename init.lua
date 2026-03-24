840438a0592cfeb467b8463aedbbe0c9bcc54dd8
司芳源 <sify21@163.com>
2026-03-24 18:22:33 +0800

    nvim-treesitter textobjects

--- init.lua
+++ init.lua
@@ -21,6 +21,15 @@ vim.lsp.enable("rust_analyzer")
 -- nvim-treesitter parsers
 require('nvim-treesitter').install { 'rust', 'python', 'html', 'latex', 'yaml', 'java' }
 
+-- nvim-treesitter textobjects
+local _select = require "nvim-treesitter-textobjects.select"
+vim.keymap.set({ "x", "o" }, "af", function() _select.select_textobject("@function.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "if", function() _select.select_textobject("@function.inner", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ac", function() _select.select_textobject("@class.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ic", function() _select.select_textobject("@class.inner", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ab", function() _select.select_textobject("@block.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ib", function() _select.select_textobject("@block.inner", "textobjects") end)
+
 -- colorscheme
 vim.cmd("colorscheme catppuccin-nvim")
 
@@ -30,4 +39,5 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fl', builtin.lsp_document_symbols, { desc = 'Telescope LSP document symbols' })
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope LSP document symbols' })
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Telescope LSP references' })
